047005d463bced3a7beb73127aab8a47f156e5f1
Sofiia_Halapova <Sonya05112014@gmail.com>
2023-02-21 19:35:36 +0200

    add user deletion and user editing

--- src/pages/dashboard/users/users.js
+++ src/pages/dashboard/users/users.js
@@ -14,11 +14,12 @@ import {
     DialogContentText,
     TextField,
     Grid,
-    DialogContent, DialogTitle
+    DialogContent, DialogTitle, IconButton
 } from "@mui/material";
 import React, {useState} from "react";
 import {Field, Form, Formik} from "formik";
 import * as Yup from "yup";
+import {Delete, Edit} from "@mui/icons-material";
 
 function createData(
     firstName: string,
@@ -29,7 +30,7 @@ function createData(
     return {firstName, lastName, email, isActive};
 }
 
-const rows = [
+const rowsList = [
     createData('Daria', "DariaSurname", "daria@gmail.com", true),
     createData('Sofiia', "SofiiaSurname", "sofiia@gmail.com", false),
     createData('Jane', "JaneSurname", "jane@gmail.com", false),
@@ -51,17 +52,92 @@ function getStatus(isActive) {
     }
 }
 
+function getUserForm(errors, touched) {
+    return (
+        <Grid container spacing={3} direction={'column'} justify={'center'}
+              alignItems={'center'}>
+            <Grid item xs={12}>
+                <Field
+                    name='firstName'
+                    type='text'
+                    as={TextField}
+                    variant='outlined'
+                    color='primary'
+                    label='Name'
+                    error={Boolean(errors.firstName) && Boolean(touched.firstName)}
+                    helperText={Boolean(touched.firstName) && errors.firstName}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <Field
+                    name='lastName'
+                    type='text'
+                    as={TextField}
+                    variant='outlined'
+                    color='primary'
+                    label="Surname"
+                    error={Boolean(errors.lastName) && Boolean(touched.lastName)}
+                    helperText={Boolean(touched.lastName) && errors.lastName}
+                />
+
+            </Grid>
+            <Grid item xs={12}>
+                <Field
+                    name='email'
+                    type='email'
+                    as={TextField}
+                    variant='outlined'
+                    color='primary'
+                    label='Email'
+                    error={Boolean(errors.email) && Boolean(touched.email)}
+                    helperText={Boolean(touched.email) && errors.email}
+                />
+            </Grid>
+        </Grid>
+    );
+}
+
 function Users() {
 
-    const [open, setOpen] = useState(false);
+    const [openAddUserDialog, setOpenAddUserDialog] = useState(false);
 
-    const handleDialogOpen = () => {
-        setOpen(true);
+    const addUserDialogOpen = () => {
+        setOpenAddUserDialog(true);
     }
 
-    const handleDialogClose = () => {
-        setOpen(false);
+    const handleAddUserDialogClose = () => {
+        setOpenAddUserDialog(false);
     }
+
+    const [openDeleteUserDialog, setOpenDeleteUserDialog] = useState(false);
+
+    const[getIndex, setIndex] = useState(-1);
+    const deleteUserDialogOpen = (index) => {
+        setIndex(index);
+        setOpenDeleteUserDialog(true);
+    }
+
+    const deleteUserDialogClose = () => {
+        setIndex(-1);
+        setOpenDeleteUserDialog(false);
+    }
+
+    const [openEditDialog, setOpenEditDialog] = useState(false);
+
+    const[getRow, setRow] = useState('');
+
+    const editDialogOpen = (row, index) => {
+        setIndex(index);
+        setRow(row);
+        setOpenEditDialog(true);
+    }
+
+    const editDialogClose = () => {
+        setIndex(-1);
+        setRow('');
+        setOpenEditDialog(false);
+    }
+
 
     const validationSchema = Yup.object().shape({
         firstName: Yup.string().required('Please enter your name'),
@@ -69,17 +145,29 @@ function Users() {
         email: Yup.string().required('Please enter email').email('Invalid email address')
     });
 
-    const initialValues = {
-        firstName: '',
-        lastName: '',
-        email: '',
+    const addNewValue = (name, lastname, email) => {
+        const data = createData(name, lastname, email, true);
+        rowsList.push(data);
     }
 
-    const addNewValue = (name, lastname, email) => {
-        let active = true;
-        const data = createData(name, lastname, email, active);
-        rows.push(data);
+
+    const handleChangeValue = (firstName, lastName, email, index) => {
+        let rows = getRows;
+        rows[index] = createData(firstName, lastName, email, true);
+        setRows(rows);
     }
+
+    const [getRows, setRows] = useState(rowsList)
+
+    const handleDeleteRow = (number) => {
+        let rowCopy = [...getRows];
+        rowCopy = rowCopy.filter(
+            (item, index) => index !== number
+        );
+        setRows(rowCopy);
+        deleteUserDialogClose();
+    }
+
 
     return (
         <Box>
@@ -89,7 +177,7 @@ function Users() {
                     <h3>Users</h3>
                 </span>
                 <Button
-                    onClick={handleDialogOpen}
+                    onClick={addUserDialogOpen}
                     variant="outlined"
                     style={{
                         height: '50%',
@@ -99,8 +187,8 @@ function Users() {
                 >
                     Add User
                 </Button>
-                <Dialog open={open}
-                        onClose={handleDialogClose}
+                <Dialog open={openAddUserDialog}
+                        onClose={handleAddUserDialogClose}
                         PaperProps={{onClick: e => e.stopPropagation()}}
                 >
                     <DialogTitle>Add User</DialogTitle>
@@ -109,58 +197,22 @@ function Users() {
                             To add user fill in required information.
                         </DialogContentText>
                         <Formik
-                            initialValues={initialValues}
+                            initialValues={{
+                                firstName: '',
+                                lastName: '',
+                                email: '',
+                            }}
                             validationSchema={validationSchema}
                             onSubmit={values => {
                                 addNewValue(values.firstName, values.lastName, values.email);
-                                handleDialogClose();
+                                handleAddUserDialogClose();
                             }}
                         >
                             {({errors, touched, isValid}) => (
-
                                 <Form>
-                                    <Grid container spacing={3} direction={'column'} justify={'center'}
-                                          alignItems={'center'}>
-                                        <Grid item xs={12}>
-                                            <Field
-                                                name='firstName'
-                                                type='text'
-                                                as={TextField}
-                                                variant='outlined'
-                                                color='primary'
-                                                label='Name'
-                                                error={Boolean(errors.firstName) && Boolean(touched.firstName)}
-                                                helperText={Boolean(touched.firstName) && errors.firstName}
-                                            />
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <Field
-                                                name='lastName'
-                                                type='text'
-                                                as={TextField}
-                                                variant='outlined'
-                                                color='primary'
-                                                label="Surname"
-                                                error={Boolean(errors.lastName) && Boolean(touched.lastName)}
-                                                helperText={Boolean(touched.lastName) && errors.lastName}
-                                            />
-
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <Field
-                                                name='email'
-                                                type='email'
-                                                as={TextField}
-                                                variant='outlined'
-                                                color='primary'
-                                                label='Email'
-                                                error={Boolean(errors.email) && Boolean(touched.email)}
-                                                helperText={Boolean(touched.email) && errors.email}
-                                            />
-                                        </Grid>
-                                    </Grid>
+                                    {getUserForm(errors, touched)}
                                     <DialogActions>
-                                        <Button onClick={handleDialogClose}>Cancel</Button>
+                                        <Button onClick={handleAddUserDialogClose}>Cancel</Button>
                                         <Button type='submit' disabled={!isValid}>Add Users</Button>
                                     </DialogActions>
                                 </Form>
@@ -174,21 +226,83 @@ function Users() {
                     <TableHead>
                         <TableRow>
                             <TableCell>First name</TableCell>
-                            <TableCell align="right">Surname</TableCell>
-                            <TableCell align="right">Email</TableCell>
-                            <TableCell align="right">Activity</TableCell>
+                            <TableCell align="center">Surname</TableCell>
+                            <TableCell align="center">Email</TableCell>
+                            <TableCell align="center">Activity</TableCell>
+                            <TableCell align="center">Actions</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row, index) => (
+                        {getRows.map((row, index) => (
                             <TableRow key={index}>
                                 <TableCell>{row.firstName}</TableCell>
-                                <TableCell align="right">{row.lastName}</TableCell>
-                                <TableCell align="right">{row.email}</TableCell>
-                                <TableCell align="right">{getStatus(row.isActive)}</TableCell>
+                                <TableCell align="center">{row.lastName}</TableCell>
+                                <TableCell align="center">{row.email}</TableCell>
+                                <TableCell align="center">{getStatus(row.isActive)}</TableCell>
+                                <TableCell align="center">
+                                    <IconButton
+                                        onClick={() => deleteUserDialogOpen(index)}
+                                    >
+                                        <Delete/>
+                                    </IconButton>
+                                    <IconButton
+                                        onClick={() => editDialogOpen(row, index)}
+                                    >
+                                        <Edit/>
+                                    </IconButton>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
+                    <Dialog open={openDeleteUserDialog}
+                            onClose={deleteUserDialogClose}
+                            PaperProps={{onClick: e => e.stopPropagation()}}
+                    >
+                        <DialogTitle align='center'>
+                            Confirm Delete
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogActions>
+                                <Button onClick={deleteUserDialogClose}>Cancel</Button>
+                                <Button onClick={() => handleDeleteRow(getIndex)}>Delete</Button>
+                            </DialogActions>
+                        </DialogContent>
+
+                    </Dialog>
+                    <Dialog open={openEditDialog}
+                            onClose={editDialogClose}
+                            PaperProps={{onClick: e => e.stopPropagation()}}
+                    >
+                        <DialogTitle align='center'>Edit User Information</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText sx={{p: 3}}>
+                                To edit user information fill in the form.
+                            </DialogContentText>
+                            <Formik
+                                initialValues={{
+                                    firstName: getRow.firstName,
+                                    lastName: getRow.lastName,
+                                    email: getRow.email,
+                                }}
+                                validationSchema={validationSchema}
+                                onSubmit={values => {
+                                    handleChangeValue(values.firstName, values.lastName, values.email, getIndex);
+                                    editDialogClose();
+                                }}
+                            >
+                                {({errors, touched, isValid}) => (
+
+                                    <Form>
+                                        {getUserForm(errors, touched)}
+                                        <DialogActions>
+                                            <Button onClick={editDialogClose}>Cancel</Button>
+                                            <Button type='submit' disabled={!isValid}>Edit</Button>
+                                        </DialogActions>
+                                    </Form>
+                                )}
+                            </Formik>
+                        </DialogContent>
+                    </Dialog>
                 </Table>
             </TableContainer>
         </Box>
